367903d7d876175603368825d3a12d9d3c8da8a5
Santiago  Hernandez <smhg15@gmail.com>
2023-12-31 19:05:11 +0100

    course completed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,7 +9,12 @@ import {
   LatestInvoicesSkeleton,
   CardsSkeleton
 } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
  
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
+
 export default async function Page() {
   // Remove `const latestInvoices = await fetchLatestInvoices()`
   const {
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,6 +10,8 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { updateInvoice } from '@/app/lib/actions';
+import { useFormState } from 'react-dom';
+
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,9 +20,11 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-  return (
-    <form action={updateInvoiceWithId}>
+  const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
+ 
+  return <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -120,6 +124,5 @@ export default function EditInvoiceForm({
         </Link>
         <Button type="submit">Edit Invoice</Button>
       </div>
-    </form>
-  );
+    </form>;
 }
